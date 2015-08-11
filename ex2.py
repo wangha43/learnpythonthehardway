61183e3885f9055084ddab45d7d16ad609c03bbd
wangha43@163.com <wangha43@163.com>
2015-08-11 13:56:47 +0800

    second and third

---
+++ ex2.py
@@ -0,0 +1,7 @@
+# A comment
+# ignored by python
+print "I could have code like this." #this comment after is ignored
+#disable
+#print "this won't run"
+
+print "this will run"
---
+++ ex3.py
@@ -0,0 +1,18 @@
+print "I will now count my chickens"
+
+print "hens",25+30/6
+print "Roosters",100-25*3%4
+
+print "Now I will count the eggs:"
+print 3+2+1-5+4%2-1/4+6
+
+print "true?3+2<5-7"
+print "what is 3+2?",3+2
+print "what is 5-7?",5-7
+
+print "it shall be false"
+
+print "how about some more"
+print "greater?",5>-2
+print "greater?",5>=-2
+print "less?",5<=-2
