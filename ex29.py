1621124e27fa34d13fe2c80a363b925f4b33c238
jackerloong <jackerloong@gmail.com>
2015-10-24 23:29:21 +0800

    修改

--- ex29.py
+++ ex29.py
@@ -1,14 +1,15 @@
+#-*-coding:utf-8-*-
 people = 20
 cats = 30
 dogs = 15
 if people < cats:
-    print "Too many cats! The world is doomed!"
+    print "人比猫少"
 
 if people > cats:
-    print "Not many cats! The world is saved!"
+    print "人类赢了"
 
 if people < dogs:
-    print "The world is drooled on!"
+    print "狗多了"
 
 if people > dogs:
     print "The world is dry!"
